8183df32b9eaa8365aec3833df783ed319fe5bed
seungmin <mirame@nextree.co.kr>
2020-01-13 14:42:28 +0900

    https://velopert.com/3346 예제 학습 1차 기능 완성

---
+++ redux-counter/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './index.css';
+
+// Redux
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+//store
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
