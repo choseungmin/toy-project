77873082acecce47745fdfaf15a355a764293da4
seungmin <mirame@nextree.co.kr>
2020-01-13 09:45:57 +0900

    redux-counter init

--- nodejs-zimcarry-scrap/src/scrapZimcarry.js
+++ nodejs-zimcarry-scrap/src/scrapZimcarry.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const log = console.log;
 
 // let page = 1;
 // let maxPage = 1;
@@ -23,10 +22,10 @@ getAjax(1).then(result => {
   // const $rooms = cheerio.load(html.data.rooms_list);
   const $ = cheerio.load(result.data.paging);
 
-  maxPage = $("a.last").attr('onclick').match(/\d+/g).map(Number)[0];
+  const maxPage = $("a.last").attr('onclick').match(/\d+/g).map(Number)[0];
   return maxPage;
 
-}).then(maxPage => {
+}).then((maxPage = 1) => {
   console.log("maxPage:", maxPage)
   for(var i=0; i<maxPage; i++) {
     getAjax(i+1).then(result => {
@@ -35,24 +34,25 @@ getAjax(1).then(result => {
       const $roomList = $("div.hotel-item");
 
       $roomList.each((index, item) => {
-        // 숙소명
-        console.log($(item).find("p.name").text());
-        // 주소
-        console.log($(item).find("span").text());
+        // 숙소명, 주소
+        console.log(
+          $(item).find("p.name").text(), '  ',
+          $(item).find("span").text()
+        );
 
         // 위도,경도
         // console.log($(item).find("a").attr("href"));
-        const param = [];
+        /*const param = [];
         const params = $(item).find("a").attr("href").split("&");
         let key, value;
         params.map((item, index) => {
           key = params[index].split("=")[0];
           value = params[index].split("=")[1];
-          // if(key == 'lng' || key == 'lat') {
+          if(key == 'lng' || key == 'lat') {
             param[key] = value;
-          // }
+          }
         })
-        console.log(param)
+        console.log(param)*/
       })
 
 
