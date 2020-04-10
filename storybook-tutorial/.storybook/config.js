5dfb114df1145d7bf4cdb63a0a436c3865af0bb9
seungmin <>
2020-04-10 09:52:10 +0900

    story book tutirial

---
+++ storybook-tutorial/.storybook/config.js
@@ -0,0 +1,6 @@
+import { configure } from '@storybook/react';
+
+// automatically import all files ending in *.stories.js
+// configure(require.context('../stories', true, /\.stories\.js$/), module);
+
+configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module);
